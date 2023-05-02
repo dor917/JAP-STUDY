f25836ad558c3bca35ddf01822476c9ab3ce37d2
dor917 <donha917@naver.com>
2023-05-02 19:41:16 +0900

    Initial commit

---
+++ src/main/java/com/plateer/yami/YamiApplication.java
@@ -0,0 +1,13 @@
+package com.plateer.yami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YamiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plateer/yami/YamiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plateer.yami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
